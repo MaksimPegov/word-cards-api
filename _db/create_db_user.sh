0bd2de7ffb77cc6ebba118a355eacf09e73e5efa
Maksim Pegov <maksim.peg@gmail.com>
2024-03-18 21:10:09 +0100

    Database cluster with pgpool implementation

--- _db/create_db_user.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-psql -h localhost -U postgres -p 5432 -c "CREATE USER postgres WITH PASSWORD 'passwords';"
-psql -h localhost -U postgres -p 5432 -c "GRANT ALL PRIVILEGES ON DATABASE usersdb TO postgres;"
