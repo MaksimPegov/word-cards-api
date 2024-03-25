6a60a9eeebb7a028ea9831b0691a035d12376548
Maksim Pegov <maksim.peg@gmail.com>
2024-03-25 10:50:18 +0100

    [#16] API gateway implementation

---
+++ api-gateway/src/main/java/com/maksimpegov/apigateway/config/AuthWebFilter.java
@@ -0,0 +1,76 @@
+package com.maksimpegov.apigateway.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthWebFilter implements GlobalFilter, Ordered {
+	private JwtService jwtService;
+
+	final Logger logger = LoggerFactory.getLogger(AuthWebFilter.class);
+
+	@Autowired
+	public AuthWebFilter(JwtService jwtService) {
+		this.jwtService = jwtService;
+	}
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		ServerHttpRequest request = exchange.getRequest();
+		ServerHttpResponse response = exchange.getResponse();
+		HttpHeaders headers = request.getHeaders();
+		String headerName = "Authorization";
+		String newHeader = "userId";
+
+		logger.info("New request to: " + request.getPath());
+
+		// Allow the request for login, register and password change
+		String requestUri = request.getPath().toString();
+		if (requestUri.contains("login") || requestUri.contains("register") || requestUri.contains("password")) {
+			return chain.filter(exchange);
+		}
+
+		// If the header is missing, abort the request
+		if (!request.getHeaders().containsKey(headerName)) {
+			response.setStatusCode(HttpStatus.BAD_REQUEST);
+			return response.setComplete();
+		}
+
+		// If the header value is not a valid JWT, abort the request
+		String existingHeader = headers.getFirst(headerName);
+		if (!jwtService.isTokenValid(existingHeader)) {
+			response.setStatusCode(HttpStatus.UNAUTHORIZED);
+			return response.setComplete();
+		}
+		logger.info("Security check passed");
+
+		logger.info("Headers before:" + headers);
+
+		// User ID is extracted from the JWT
+		String userId = jwtService.getUserId(existingHeader);
+
+		// Create a new ServerHttpRequest with the modified headers
+		ServerHttpRequest modifiedRequest = request.mutate().headers(httpHeaders -> httpHeaders.add(newHeader, userId)).build();
+
+		logger.info("Updated headers: " + modifiedRequest.getHeaders());
+
+		// Continue the filter chain with the modified request
+		return chain.filter(exchange.mutate().request(modifiedRequest).build());
+	}
+
+	@Override
+	public int getOrder() {
+		return 1;
+	}
+}
---
+++ api-gateway/src/main/java/com/maksimpegov/apigateway/config/JwtService.java
@@ -0,0 +1,60 @@
+package com.maksimpegov.apigateway.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+public class JwtService {
+
+	private static final SignatureAlgorithm SIGNATURE_ALGORITHM = SignatureAlgorithm.HS256;
+	private static final Key SECRET_KEY = Keys.secretKeyFor(SIGNATURE_ALGORITHM);
+
+	public String generateToken(String userId) {
+		return Jwts.builder()
+				.setSubject(userId)
+				.setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + 7 * 24 * 60 * 60 * 1000)) // 1 week
+				.signWith(SECRET_KEY, SIGNATURE_ALGORITHM)
+				.compact();
+	}
+
+	public String getUserId(String jwt) {
+		return getClaims(jwt, Claims::getSubject);
+	}
+
+	public <T> T getClaims(String token, Function<Claims, T> claimResolver) {
+		final Claims claims = getAllClaims(token);
+		return claimResolver.apply(claims);
+	}
+
+	public Boolean isTokenValid(String jwt) {
+		try {
+			return !isTokenExpired(jwt);
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	private boolean isTokenExpired(String jwt) {
+		return extractExpiration(jwt).before(new Date());
+	}
+
+	private Date extractExpiration(String jwt) {
+		return getClaims(jwt, Claims::getExpiration);
+	}
+
+	private Claims getAllClaims(String jwt) {
+		return Jwts.parserBuilder()
+				.setSigningKey(SECRET_KEY)
+				.build()
+				.parseClaimsJws(jwt)
+				.getBody();
+	}
+}
